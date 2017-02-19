507f2e5491a88601478715f2ee6e90709c063d7d
Charlenni <info@wfplayer.com>
2017-02-19 18:35:44 +0100

    Added bindable property BackgroundColor

--- Mapsui.Forms/MapView.cs
+++ Mapsui.Forms/MapView.cs
@@ -1,4 +1,4 @@
-﻿using Mapsui;
+﻿using Mapsui.Forms.Extensions;
 using Mapsui.Projection;
 using System;
 using Xamarin.Forms;
@@ -10,11 +10,65 @@ namespace Mapsui.Forms
 	/// </summary>
 	public class MapView : View
 	{
-		public Map Map { get; set; }
+		/// <summary>
+		/// Privates
+		/// </summary>
+		internal Map nativeMap;
 
 		public MapView()
 		{
 			Map = new Map();
 		}
+
+		public Map Map
+		{
+			get
+			{
+				return nativeMap;
+			}
+			set
+			{
+				if (value != nativeMap)
+				{
+					nativeMap = value;
+					// Set defaults
+					nativeMap.BackColor = BackgroundColor.ToMapsuiColor();
+					// TODO
+					// Something changed, so update map
+				}
+			}
+		}
+
+		/// <summary>
+		/// Properties
+		/// </summary>
+		 
+		public new Color BackgroundColor
+		{
+			get
+			{
+				return (Color)GetValue(BackgroundColorProperty);
+			}
+			set
+			{
+				if (value != null && nativeMap.BackColor != value.ToMapsuiColor())
+				{
+					nativeMap.BackColor = value.ToMapsuiColor();
+					SetValue(BackgroundColorProperty, value);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Bindings
+		/// </summary>
+		 
+		public new static readonly BindableProperty BackgroundColorProperty = BindableProperty.Create(
+																propertyName: nameof(BackgroundColor),
+																returnType: typeof(Color),
+																declaringType: typeof(MapView),
+																defaultValue: Color.White,
+																defaultBindingMode: BindingMode.TwoWay,
+																propertyChanged: null);
 	}
 }
