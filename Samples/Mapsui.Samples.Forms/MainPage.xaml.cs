3cd545b0bd11951897f49b947bb033bdfe6a56b6
Charlenni <info@wfplayer.com>
2017-02-19 09:59:24 +0100

    Added Xamarin.Forms support for Android and iOS.

---
+++ Samples/Mapsui.Samples.Forms/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Mapsui.Forms;
+using Mapsui.Geometries;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using Mapsui.Providers;
+using Mapsui.Samples.Common.Maps;
+using Mapsui.Styles;
+using Xamarin.Forms;
+
+namespace Mapsui.Samples.Forms
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			mapView.Map = BingSample.CreateMap();
+//			mapView.Map = InfoLayersSample.CreateMap();
+//			mapView.Map = LabelsSample.CreateMap();
+		}
+	}
+}
