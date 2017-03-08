ea477734d8533bc3f704f0478a5b50a80d008a6b
Charlenni <info@wfplayer.com>
2017-03-08 10:59:01 +0100

    Revert changes for Viewport, because master changed

--- Mapsui/Map.cs
+++ Mapsui/Map.cs
@@ -30,115 +30,115 @@ using Mapsui.Utilities;
 
 namespace Mapsui
 {
-    /// <summary>
-    /// Map class
-    /// </summary>
-    public class Map : IDisposable, INotifyPropertyChanged
-    {
-        private LayerCollection _layers = new LayerCollection();
-        private bool _lock;
+	/// <summary>
+	/// Map class
+	/// </summary>
+	public class Map : IDisposable, INotifyPropertyChanged
+	{
+		private LayerCollection _layers = new LayerCollection();
+		private bool _lock;
 		private Color _backColor = Color.White;
 
 		/// <summary>
-		/// Initializes a new map with given Viewport
+		/// Initializes a new map
 		/// </summary>
-		public Map(IViewport viewport = null)
+		public Map()
 		{
 			BackColor = Color.White;
 			Layers = new LayerCollection();
-			Viewport = viewport != null ? viewport : new Viewport { Center = { X = double.NaN, Y = double.NaN }, Resolution = double.NaN };
+			Viewport = new Viewport { Center = { X = double.NaN, Y = double.NaN }, Resolution = double.NaN };
 		}
 
 		/// <summary>
 		/// When Lock is true the map view will not respond to touch input.
 		/// </summary>
 		public bool Lock
-        {
-            get { return _lock; }
-            set
-            {
-                if (_lock == value) return;
-                _lock = value;
-                OnPropertyChanged(nameof(Lock));
-            }
-        }
+		{
+			get { return _lock; }
+			set
+			{
+				if (_lock == value) return;
+				_lock = value;
+				OnPropertyChanged(nameof(Lock));
+			}
+		}
 
-        public string CRS { get; set; }
+		public string CRS { get; set; }
 
-        /// <summary>
-        /// The maps coördinate system
-        /// </summary>
-        public ITransformation Transformation { get; set; }
+		/// <summary>
+		/// The maps coördinate system
+		/// </summary>
+		public ITransformation Transformation { get; set; }
 
-        /// <summary>
-        /// A collection of layers. The first layer in the list is drawn first, the last one on top.
-        /// </summary>
-        public LayerCollection Layers
-        {
-            get { return _layers; }
-            private set
-            {
-                var tempLayers = _layers;
-                if (tempLayers != null)
-                {
-                    _layers.LayerAdded -= LayersLayerAdded;
-                    _layers.LayerRemoved -= LayersLayerRemoved;
-                }
-                _layers = value;
-                _layers.LayerAdded += LayersLayerAdded;
-                _layers.LayerRemoved += LayersLayerRemoved;
-            }
-        }
-        
-        public IList<ILayer> InfoLayers { get; private set; } = new List<ILayer>();
+		/// <summary>
+		/// A collection of layers. The first layer in the list is drawn first, the last one on top.
+		/// </summary>
+		public LayerCollection Layers
+		{
+			get { return _layers; }
+			private set
+			{
+				var tempLayers = _layers;
+				if (tempLayers != null)
+				{
+					_layers.LayerAdded -= LayersLayerAdded;
+					_layers.LayerRemoved -= LayersLayerRemoved;
+				}
+				_layers = value;
+				_layers.LayerAdded += LayersLayerAdded;
+				_layers.LayerRemoved += LayersLayerRemoved;
+			}
+		}
 
-        public IList<ILayer> HoverInfoLayers { get; private set; } = new List<ILayer>();
+		public IList<ILayer> InfoLayers { get; private set; } = new List<ILayer>();
 
-        public IViewport Viewport { get; set; }
+		public IList<ILayer> HoverInfoLayers { get; private set; } = new List<ILayer>();
 
-        public void NavigateTo(BoundingBox extent, ScaleMethod scaleMethod = ScaleMethod.Fit)
-        {
-            Viewport.Resolution = ZoomHelper.DetermineResolution(
-                extent.Width, extent.Height, Viewport.Width, Viewport.Height, scaleMethod);
-            Viewport.Center = extent.GetCentroid();
+		public Viewport Viewport { get; }
 
-            OnRefreshGraphics();
-            ViewChanged(true);
-        }
+		public void NavigateTo(BoundingBox extent, ScaleMethod scaleMethod = ScaleMethod.Fit)
+		{
+			Viewport.Resolution = ZoomHelper.DetermineResolution(
+				extent.Width, extent.Height, Viewport.Width, Viewport.Height, scaleMethod);
+			Viewport.Center = extent.GetCentroid();
 
-        public void NavigateTo(double resolution)
-        {
-            Viewport.Resolution = resolution;
-            OnRefreshGraphics();
-            ViewChanged(true);
-        }
+			OnRefreshGraphics();
+			ViewChanged(true);
+		}
 
-        public void NavigateTo(Point center)
-        {
-            Viewport.Center = center;
-            OnRefreshGraphics();
-            ViewChanged(true);
-        }
+		public void NavigateTo(double resolution)
+		{
+			Viewport.Resolution = resolution;
+			OnRefreshGraphics();
+			ViewChanged(true);
+		}
 
-        public void NavigateTo(double x, double y)
-        {
-            Viewport.Center.X = x;
-            Viewport.Center.Y = y;
-            OnRefreshGraphics();
-            ViewChanged(true);
-        }
+		public void NavigateTo(Point center)
+		{
+			Viewport.Center = center;
+			OnRefreshGraphics();
+			ViewChanged(true);
+		}
 
-        public void RotateTo(double rotation)
-        {
-            Viewport.Rotation = rotation;
-            OnRefreshGraphics();
-            ViewChanged(true);
-        }
+		public void NavigateTo(double x, double y)
+		{
+			Viewport.Center.X = x;
+			Viewport.Center.Y = y;
+			OnRefreshGraphics();
+			ViewChanged(true);
+		}
 
-        /// <summary>
-        /// Map background color (defaults to white)
-        ///  </summary>
-        public Color BackColor
+		public void RotateTo(double rotation)
+		{
+			Viewport.Rotation = rotation;
+			OnRefreshGraphics();
+			ViewChanged(true);
+		}
+
+		/// <summary>
+		/// Map background color (defaults to white)
+		///  </summary>
+		public Color BackColor
 		{
 			get { return _backColor; }
 			set
@@ -148,135 +148,135 @@ namespace Mapsui
 				_backColor = value;
 				OnRefreshGraphics();
 			}
-		} 
+		}
 
-        /// <summary>
-        /// Gets the extents of the map based on the extents of all the layers in the layers collection
-        /// </summary>
-        /// <returns>Full map extents</returns>
-        public BoundingBox Envelope
-        {
-            get
-            {
-                if (_layers.Count == 0) return null;
+		/// <summary>
+		/// Gets the extents of the map based on the extents of all the layers in the layers collection
+		/// </summary>
+		/// <returns>Full map extents</returns>
+		public BoundingBox Envelope
+		{
+			get
+			{
+				if (_layers.Count == 0) return null;
 
-                BoundingBox bbox = null;
-                foreach (var layer in _layers)
-                {
-                    bbox = bbox == null ? layer.Envelope : bbox.Join(layer.Envelope);
-                }
-                return bbox;
-            }
-        }
+				BoundingBox bbox = null;
+				foreach (var layer in _layers)
+				{
+					bbox = bbox == null ? layer.Envelope : bbox.Join(layer.Envelope);
+				}
+				return bbox;
+			}
+		}
 
-        public IList<double> Resolutions 
-        {
-            get 
-            { 
-                var baseLayer = Layers.FirstOrDefault(l => l.Enabled && l is ITileLayer) as ITileLayer;
-                if (baseLayer?.Schema == null) return new List<double>();
-                return baseLayer.Schema.Resolutions.Select(r => r.Value.UnitsPerPixel).ToList();
-            }
-        }
+		public IList<double> Resolutions
+		{
+			get
+			{
+				var baseLayer = Layers.FirstOrDefault(l => l.Enabled && l is ITileLayer) as ITileLayer;
+				if (baseLayer?.Schema == null) return new List<double>();
+				return baseLayer.Schema.Resolutions.Select(r => r.Value.UnitsPerPixel).ToList();
+			}
+		}
 
-        /// <summary>
-        /// Disposes 
-        /// the map object
-        /// </summary>
-        public void Dispose()
-        {
-            AbortFetch();
-            _layers.Clear();
-        }
+		/// <summary>
+		/// Disposes 
+		/// the map object
+		/// </summary>
+		public void Dispose()
+		{
+			AbortFetch();
+			_layers.Clear();
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        /// <summary>
-        /// DataChanged should be triggered by any data changes of any of the child layers
-        /// </summary>
-        public event DataChangedEventHandler DataChanged;
-        public event EventHandler RefreshGraphics;
-        public event EventHandler<MouseInfoEventArgs> Info;
+		/// <summary>
+		/// DataChanged should be triggered by any data changes of any of the child layers
+		/// </summary>
+		public event DataChangedEventHandler DataChanged;
+		public event EventHandler RefreshGraphics;
+		public event EventHandler<MouseInfoEventArgs> Info;
 
-        private void LayersLayerRemoved(ILayer layer)
-        {
-            layer.AbortFetch();
+		private void LayersLayerRemoved(ILayer layer)
+		{
+			layer.AbortFetch();
 
-            layer.DataChanged -= LayerDataChanged;
-            layer.PropertyChanged -= LayerPropertyChanged;
+			layer.DataChanged -= LayerDataChanged;
+			layer.PropertyChanged -= LayerPropertyChanged;
 
-            OnPropertyChanged(nameof(Layers));
-        }
+			OnPropertyChanged(nameof(Layers));
+		}
 
-        public void InvokeInfo(Point screenPosition)
-        {
-            if (Info == null) return;
-            var eventArgs = InfoHelper.GetInfoEventArgs(this, screenPosition, InfoLayers);
-            if (eventArgs != null) Info?.Invoke(this, eventArgs);
-        }
+		public void InvokeInfo(Point screenPosition)
+		{
+			if (Info == null) return;
+			var eventArgs = InfoHelper.GetInfoEventArgs(this, screenPosition, InfoLayers);
+			if (eventArgs != null) Info?.Invoke(this, eventArgs);
+		}
 
-        private void LayersLayerAdded(ILayer layer)
-        {
-            layer.DataChanged += LayerDataChanged;
-            layer.PropertyChanged += LayerPropertyChanged;
+		private void LayersLayerAdded(ILayer layer)
+		{
+			layer.DataChanged += LayerDataChanged;
+			layer.PropertyChanged += LayerPropertyChanged;
 
-            layer.Transformation = Transformation;
-            layer.CRS = CRS;
-            OnPropertyChanged(nameof(Layers));
-        }
+			layer.Transformation = Transformation;
+			layer.CRS = CRS;
+			OnPropertyChanged(nameof(Layers));
+		}
 
-        private void LayerPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            OnPropertyChanged(sender, e.PropertyName);
-        }
+		private void LayerPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			OnPropertyChanged(sender, e.PropertyName);
+		}
 
-        private void OnRefreshGraphics()
-        {
-            RefreshGraphics?.Invoke(this, EventArgs.Empty);
-        }
+		private void OnRefreshGraphics()
+		{
+			RefreshGraphics?.Invoke(this, EventArgs.Empty);
+		}
 
-        private void OnPropertyChanged(object sender, string propertyName)
-        {
-            PropertyChanged?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
-        }
+		private void OnPropertyChanged(object sender, string propertyName)
+		{
+			PropertyChanged?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+		}
 
-        private void OnPropertyChanged(string name)
-        {
-            OnPropertyChanged(this, name);
-        }
+		private void OnPropertyChanged(string name)
+		{
+			OnPropertyChanged(this, name);
+		}
 
-        private void LayerDataChanged(object sender, DataChangedEventArgs e)
-        {
-            OnDataChanged(sender, e);
-        }
-        
-        private void OnDataChanged(object sender, DataChangedEventArgs e)
-        {
-            DataChanged?.Invoke(sender, e);
-        }
+		private void LayerDataChanged(object sender, DataChangedEventArgs e)
+		{
+			OnDataChanged(sender, e);
+		}
 
-        public void AbortFetch()
-        {
-            foreach (var layer in _layers.ToList())
-            {
-                layer.AbortFetch();
-            }
-        }
+		private void OnDataChanged(object sender, DataChangedEventArgs e)
+		{
+			DataChanged?.Invoke(sender, e);
+		}
 
-        public void ViewChanged(bool majorChange)
-        {
-            foreach (var layer in _layers.ToList())
-            {
-                layer.ViewChanged(majorChange, Viewport.Extent, Viewport.RenderResolution);
-            }
-        }
+		public void AbortFetch()
+		{
+			foreach (var layer in _layers.ToList())
+			{
+				layer.AbortFetch();
+			}
+		}
 
-        public void ClearCache()
-        {
-            foreach (var layer in _layers)
-            {
-                layer.ClearCache();
-            }
-        }
-    }
+		public void ViewChanged(bool majorChange)
+		{
+			foreach (var layer in _layers.ToList())
+			{
+				layer.ViewChanged(majorChange, Viewport.Extent, Viewport.RenderResolution);
+			}
+		}
+
+		public void ClearCache()
+		{
+			foreach (var layer in _layers)
+			{
+				layer.ClearCache();
+			}
+		}
+	}
 }
--- Mapsui/Viewport.cs
+++ Mapsui/Viewport.cs
@@ -15,239 +15,247 @@
 // along with SharpMap; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
 
+using System;
 using Mapsui.Geometries;
 using Mapsui.Utilities;
 
 namespace Mapsui
 {
-    public class Viewport : IViewport
-    {
-        private readonly BoundingBox _extent;
-        private Quad _windowExtent;
-        private double _height;
-        private double _resolution;
-        private double _width;
-        private double _rotation;
-        private readonly NotifyingPoint _center = new NotifyingPoint();
-        private bool _modified = true;
+	public class Viewport : IViewport
+	{
+		public event EventHandler ViewportChanged;
 
-        public Viewport()
-        {
-            RenderResolutionMultiplier = 1;
-            _extent = new BoundingBox(0, 0, 0, 0);
-            _windowExtent = new Quad();
-            _center.PropertyChanged += (sender, args) => _modified = true;
-        }
-        
-        public Viewport(Viewport viewport) : this()
-        {
-            _resolution = viewport._resolution;
-            _width = viewport._width;
-            _height = viewport._height;
-            _rotation = viewport._rotation;
-            _center.X = viewport._center.X;
-            _center.Y = viewport._center.Y;
-            if (viewport.Extent!= null) _extent = new BoundingBox(viewport.Extent);
-            if (viewport.WindowExtent != null) _windowExtent = new Quad(
-                viewport.WindowExtent.BottomLeft, viewport.WindowExtent.TopLeft,
-                viewport.WindowExtent.TopRight, viewport.WindowExtent.BottomRight);
-            _center.PropertyChanged += (sender, args) => _modified = true;
-            RenderResolutionMultiplier = viewport.RenderResolutionMultiplier;
-        }
+		private readonly BoundingBox _extent;
+		private Quad _windowExtent;
+		private double _height;
+		private double _resolution;
+		private double _width;
+		private double _rotation;
+		private readonly NotifyingPoint _center = new NotifyingPoint();
+		private bool _modified = true;
 
-        public double RenderResolutionMultiplier { get; set; }
+		public Viewport()
+		{
+			RenderResolutionMultiplier = 1;
+			_extent = new BoundingBox(0, 0, 0, 0);
+			_windowExtent = new Quad();
+			_center.PropertyChanged += (sender, args) => OnViewportChanged();
+		}
 
-        public double RenderResolution => Resolution * RenderResolutionMultiplier;
+		public Viewport(Viewport viewport) : this()
+		{
+			_resolution = viewport._resolution;
+			_width = viewport._width;
+			_height = viewport._height;
+			_rotation = viewport._rotation;
+			_center.X = viewport._center.X;
+			_center.Y = viewport._center.Y;
+			if (viewport.Extent != null) _extent = new BoundingBox(viewport.Extent);
+			if (viewport.WindowExtent != null) _windowExtent = new Quad(
+				viewport.WindowExtent.BottomLeft, viewport.WindowExtent.TopLeft,
+				viewport.WindowExtent.TopRight, viewport.WindowExtent.BottomRight);
+			RenderResolutionMultiplier = viewport.RenderResolutionMultiplier;
+		}
 
-        public Point Center
-        {
-            get { return _center; }
-            set
-            {
-                _center.X = value.X;
-                _center.Y = value.Y;
-                _modified = true;
-            }
-        }
+		private void OnViewportChanged()
+		{
+			_modified = true;
+			ViewportChanged?.Invoke(this, new EventArgs());
+		}
 
-        public double Resolution
-        {
-            get { return _resolution; }
-            set
-            {
-                _resolution = value;
-                _modified = true;
-            }
-        }
+		public double RenderResolutionMultiplier { get; set; }
 
-        public double Width
-        {
-            get { return _width; }
-            set
-            {
-                _width = value;
-                _modified = true;
-            }
-        }
+		public double RenderResolution => Resolution * RenderResolutionMultiplier;
 
-        public double Height
-        {
-            get { return _height; }
-            set
-            {
-                _height = value;
-                _modified = true;
-            }
-        }
+		public Point Center
+		{
+			get { return _center; }
+			set
+			{
+				_center.X = value.X;
+				_center.Y = value.Y;
+				OnViewportChanged();
+			}
+		}
 
-        public double Rotation
-        {
-            get { return _rotation; }
-            set
-            {
-                // normalize the value to be [0, 360)
-                _rotation = value % 360.0;
-                if (_rotation < 0)
-                    _rotation += 360.0;
-                _modified = true;
-            }
-        }
+		public double Resolution
+		{
+			get { return _resolution; }
+			set
+			{
+				_resolution = value;
+				OnViewportChanged();
+			}
+		}
 
-        public bool IsRotated => 
-            !double.IsNaN(_rotation) && _rotation > Constants.Epsilon && _rotation < 360 - Constants.Epsilon;
+		public double Width
+		{
+			get { return _width; }
+			set
+			{
+				_width = value;
+				OnViewportChanged();
+			}
+		}
 
-        public BoundingBox Extent
-        {
-            get
-            {
-                if (_modified) UpdateExtent(); 
-                return _extent;
-            }
-        }
+		public double Height
+		{
+			get { return _height; }
+			set
+			{
+				_height = value;
+				OnViewportChanged();
+			}
+		}
 
-        public Quad WindowExtent
-        {
-            get
-            {
-                if (_modified) UpdateExtent();
-                return _windowExtent;
-            }
-        }
+		public double Rotation
+		{
+			get { return _rotation; }
+			set
+			{
+				// normalize the value to be [0, 360)
+				_rotation = value % 360.0;
+				if (_rotation < 0)
+					_rotation += 360.0;
+				OnViewportChanged();
+			}
+		}
 
-        public Point WorldToScreen(Point worldPosition)
-        {
-            return WorldToScreen(worldPosition.X, worldPosition.Y);
-        }
+		public bool IsRotated =>
+			!double.IsNaN(_rotation) && _rotation > Constants.Epsilon && _rotation < 360 - Constants.Epsilon;
 
-        public Point WorldToScreenUnrotated(Point worldPosition)
-        {
-            return WorldToScreenUnrotated(worldPosition.X, worldPosition.Y);
-        }
+		public BoundingBox Extent
+		{
+			get
+			{
+				if (_modified) UpdateExtent();
+				return _extent;
+			}
+		}
 
-        public Point ScreenToWorld(Point screenPosition)
-        {
-            return ScreenToWorld(screenPosition.X, screenPosition.Y);
-        }
+		public Quad WindowExtent
+		{
+			get
+			{
+				if (_modified) UpdateExtent();
+				return _windowExtent;
+			}
+		}
 
-        public Point WorldToScreen(double worldX, double worldY)
-        {
-            var p = WorldToScreenUnrotated(worldX, worldY);
-            
-            if (IsRotated)
-            {
-                var screenCenterX = Width / 2.0;
-                var screenCenterY = Height / 2.0;
-                p = p.Rotate(-_rotation, screenCenterX, screenCenterY);
-            }
+		public Point WorldToScreen(Point worldPosition)
+		{
+			return WorldToScreen(worldPosition.X, worldPosition.Y);
+		}
 
-            return p;
-        }
+		public Point WorldToScreenUnrotated(Point worldPosition)
+		{
+			return WorldToScreenUnrotated(worldPosition.X, worldPosition.Y);
+		}
 
-        public Point WorldToScreenUnrotated(double worldX, double worldY)
-        {
-            var screenCenterX = Width / 2.0;
-            var screenCenterY = Height / 2.0;
-            var screenX = (worldX - Center.X) / _resolution + screenCenterX;
-            var screenY = (Center.Y - worldY) / _resolution + screenCenterY;
+		public Point ScreenToWorld(Point screenPosition)
+		{
+			return ScreenToWorld(screenPosition.X, screenPosition.Y);
+		}
 
-            return new Point(screenX, screenY);
-        }
+		public Point WorldToScreen(double worldX, double worldY)
+		{
+			var p = WorldToScreenUnrotated(worldX, worldY);
 
-        public Point ScreenToWorld(double screenX, double screenY)
-        {
-            var screenCenterX = Width / 2.0;
-            var screenCenterY = Height / 2.0;
+			if (IsRotated)
+			{
+				var screenCenterX = Width / 2.0;
+				var screenCenterY = Height / 2.0;
+				p = p.Rotate(-_rotation, screenCenterX, screenCenterY);
+			}
 
-            if (IsRotated)
-            {
-                var screen = new Point(screenX, screenY).Rotate(_rotation, screenCenterX, screenCenterY);
-                screenX = screen.X;
-                screenY = screen.Y;
-            }
+			return p;
+		}
 
-            var worldX = Center.X + (screenX - screenCenterX) * _resolution;
-            var worldY = Center.Y - (screenY - screenCenterY) * _resolution;
-            return new Point(worldX, worldY);
-        }
+		public Point WorldToScreenUnrotated(double worldX, double worldY)
+		{
+			var screenCenterX = Width / 2.0;
+			var screenCenterY = Height / 2.0;
+			var screenX = (worldX - Center.X) / _resolution + screenCenterX;
+			var screenY = (Center.Y - worldY) / _resolution + screenCenterY;
 
-        public void Transform(double screenX, double screenY, double previousScreenX, double previousScreenY, double deltaScale = 1)
-        {
-            var previous = ScreenToWorld(previousScreenX, previousScreenY);
-            var current = ScreenToWorld(screenX, screenY);
+			return new Point(screenX, screenY);
+		}
 
-            var newX = Center.X + previous.X - current.X;
-            var newY = Center.Y + previous.Y - current.Y;
+		public Point ScreenToWorld(double screenX, double screenY)
+		{
+			var screenCenterX = Width / 2.0;
+			var screenCenterY = Height / 2.0;
 
-            Resolution = Resolution / deltaScale;
+			if (IsRotated)
+			{
+				var screen = new Point(screenX, screenY).Rotate(_rotation, screenCenterX, screenCenterY);
+				screenX = screen.X;
+				screenY = screen.Y;
+			}
 
-            current = ScreenToWorld(screenX, screenY); // calculate current position again with adjusted resolution
-            // Zooming should be centered on the place where the map is touched. This is done with the scale correction.
-            var scaleCorrectionX = (1 - deltaScale) * (current.X - Center.X);
-            var scaleCorrectionY = (1 - deltaScale) * (current.Y - Center.Y);
-            
-            Center.X = newX - scaleCorrectionX;
-            Center.Y = newY - scaleCorrectionY;
-            _modified = true;
-        }
+			var worldX = Center.X + (screenX - screenCenterX) * _resolution;
+			var worldY = Center.Y - (screenY - screenCenterY) * _resolution;
+			return new Point(worldX, worldY);
+		}
 
-        private void UpdateExtent()
-        {
-            if (double.IsNaN(_center.X)) return;
-            if (double.IsNaN(_center.Y)) return;
-            if (double.IsNaN(_resolution)) return;
+		public void Transform(double screenX, double screenY, double previousScreenX, double previousScreenY, double deltaScale = 1)
+		{
+			var previous = ScreenToWorld(previousScreenX, previousScreenY);
+			var current = ScreenToWorld(screenX, screenY);
 
-            // calculate the window extent which is not rotate
-            var halfSpanX = _width * _resolution * 0.5;
-            var halfSpanY = _height * _resolution * 0.5;
-            var left = Center.X - halfSpanX;
-            var bottom = Center.Y - halfSpanY;
-            var right = Center.X + halfSpanX;
-            var top = Center.Y + halfSpanY;
-            _windowExtent.BottomLeft = new Point(left, bottom);
-            _windowExtent.TopLeft = new Point(left, top);
-            _windowExtent.TopRight = new Point(right, top);
-            _windowExtent.BottomRight = new Point(right, bottom);
+			var newX = Center.X + previous.X - current.X;
+			var newY = Center.Y + previous.Y - current.Y;
 
-            if (!IsRotated)
-            {
-                _extent.Min.X = left;
-                _extent.Min.Y = bottom;
-                _extent.Max.X = right;
-                _extent.Max.Y = top;
-            }
-            else
-            {
-                // Calculate the extent that will encompass a rotated viewport (slighly larger - used for tiles).
-                // Perform rotations on corner offsets and then add them to the Center point.
-                _windowExtent = _windowExtent.Rotate(-_rotation, Center.X, Center.Y);
-                var rotatedBoundingBox = _windowExtent.ToBoundingBox();
-                _extent.Min.X = rotatedBoundingBox.MinX;
-                _extent.Min.Y = rotatedBoundingBox.MinY;
-                _extent.Max.X = rotatedBoundingBox.MaxX;
-                _extent.Max.Y = rotatedBoundingBox.MaxY;
-            }
+			Resolution = Resolution / deltaScale;
 
-            _modified = false;
-        }
-    }
+			current = ScreenToWorld(screenX, screenY); // calculate current position again with adjusted resolution
+													   // Zooming should be centered on the place where the map is touched. This is done with the scale correction.
+			var scaleCorrectionX = (1 - deltaScale) * (current.X - Center.X);
+			var scaleCorrectionY = (1 - deltaScale) * (current.Y - Center.Y);
+
+			Center.X = newX - scaleCorrectionX;
+			Center.Y = newY - scaleCorrectionY;
+			OnViewportChanged();
+		}
+
+		private void UpdateExtent()
+		{
+			if (double.IsNaN(_center.X)) return;
+			if (double.IsNaN(_center.Y)) return;
+			if (double.IsNaN(_resolution)) return;
+
+			// calculate the window extent which is not rotate
+			var halfSpanX = _width * _resolution * 0.5;
+			var halfSpanY = _height * _resolution * 0.5;
+			var left = Center.X - halfSpanX;
+			var bottom = Center.Y - halfSpanY;
+			var right = Center.X + halfSpanX;
+			var top = Center.Y + halfSpanY;
+			_windowExtent.BottomLeft = new Point(left, bottom);
+			_windowExtent.TopLeft = new Point(left, top);
+			_windowExtent.TopRight = new Point(right, top);
+			_windowExtent.BottomRight = new Point(right, bottom);
+
+			if (!IsRotated)
+			{
+				_extent.Min.X = left;
+				_extent.Min.Y = bottom;
+				_extent.Max.X = right;
+				_extent.Max.Y = top;
+			}
+			else
+			{
+				// Calculate the extent that will encompass a rotated viewport (slighly larger - used for tiles).
+				// Perform rotations on corner offsets and then add them to the Center point.
+				_windowExtent = _windowExtent.Rotate(-_rotation, Center.X, Center.Y);
+				var rotatedBoundingBox = _windowExtent.ToBoundingBox();
+				_extent.Min.X = rotatedBoundingBox.MinX;
+				_extent.Min.Y = rotatedBoundingBox.MinY;
+				_extent.Max.X = rotatedBoundingBox.MaxX;
+				_extent.Max.Y = rotatedBoundingBox.MaxY;
+			}
+
+			_modified = false;
+		}
+	}
 }
